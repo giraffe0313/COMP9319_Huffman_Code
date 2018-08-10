ba30d67ea56bf3087164d3797109746c8a3e23ff
Wujian <wujian@wujian.local>
2018-08-10 15:29:40 +1000

    Initial Commit

---
+++ assignment_1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  assignment_1
+//
+//  Created by Wujian on 10/8/18.
+//  Copyright © 2018 Wujian. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
